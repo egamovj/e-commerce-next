95c7520a3e4c58d5f88b2a59a6136a8c7ba1905b
Jo'rabek Egamov <134671078+egamovj@users.noreply.github.com>
2024-02-13 18:08:42 +0500

    data fetched

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,14 +1,21 @@
-'use client'
+/* eslint-disable @next/next/no-img-element */
+"use client";
 
-import { ProductType } from '@/interfaces';
-import React, { FC } from 'react'
+import { ProductType } from "@/interfaces";
+import React, { FC } from "react";
 
-const Product: FC<{product: ProductType}> = ({product}) => {
-    console.log(product);
-    
+const Product: FC<{ product: ProductType }> = ({ product }) => {
   return (
-    <div>Product</div>
-  )
-}
+    <div className="bg-gray-100 p-6 rounded-lg">
+          <img className="h-40 rounded w-full object-cover object-center mb-6" src={product.image} alt="content" />
+          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">{product.category}</h3>
+          <div className="font-semibold flex items-center justify-between mt-4 mb-1">
+            <p className="w-44 truncate">{product.title}</p>
+            <p>${product.price}</p>
+          </div>
+          <p className="leading-relaxed text-base line-clamp-2">{product.description}</p>
+        </div>
+  );
+};
 
 export default Product;
